7dc8c5132f69b04952961361885bd3be20e7b2f5
Brice <brice.bene@gmail.com>
2018-04-25 17:37:36 +0200

    Add analytics on products & items with async week change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,36 @@
 class OrdersController < ApplicationController
+  before_action :find_orders
+  before_action :weeks_filter, only: :analytics
 
   def index
-    @orders = current_customer.orders.order(:status)
     h = {my_val_1: {my_val_2: {'my_val_3': 666}}}
     puts h.dig(:my_val_1, :my_val_2, :my_val_3)
   end
 
   def analytics
-    
+    case params[:analysis_type]
+    when 'items'
+      @items = @orders.includes(:items).map(&:items).uniq.flatten
+    else
+      @products = @orders.includes(:product).map(&:product).uniq
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def find_orders
+    @orders  = current_customer.orders
+                               .confirmed
+                               .order(created_at: :desc)
+  end
+
+  def weeks_filter
+    @from_week = params[:from_week].present? ? params[:from_week].to_i : 1
+    @to_week = params[:to_week].present? ? params[:to_week].to_i : 0
+    @orders = @orders.weeks_ago(@from_week, @to_week)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :product
+  has_many :items, through: :product
 
   enum status: [:daft, :confirmed, :canceled]
+
+  scope :weeks_ago, -> (from_week = 0, to_week = 1) { 
+    where('created_at > ? AND created_at < ?', from_week.to_i.week.ago, to_week.to_i.week.ago)
+  }
+
+  scope :confirmed, -> { where( status: 'confirmed')}
+
 end
