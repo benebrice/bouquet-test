1821f4d3278d1f8201395d9c9494034fdec5b2b4
Brice <brice.bene@gmail.com>
2018-05-07 12:18:11 +0200

    Add tests on OrdersController

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
-#require 'factories/customer_spec' 
 
 describe OrdersController, type: :controller do
+  let(:customer) { create(:customer) }
+
   describe "GET analytics" do
-    let(:customer) { create(:customer) }
+    subject { get :index }
+
+    it 'cannot access without login' do
+      expect(subject).to redirect_to(new_customer_session_path)
+    end
+
+    it 'render index view' do
+      sign_in customer
+      expect(subject).to render_template(:index)
+    end
+  end
+
+  describe "GET analytics" do
+    subject { get :analytics }
 
     let(:product) { create(:product) }
 
@@ -22,24 +36,40 @@ describe OrdersController, type: :controller do
       order1
       order2
     end
+
+    describe 'view rendering' do
+      it 'cannot access without login' do
+        expect(subject).to redirect_to(new_customer_session_path)
+      end
+
+      it 'render index view' do
+        sign_in customer
+        expect(subject).to render_template('orders/analytics')
+      end
+    end
+
+    describe 'variable assignments' do
+      it "assigns @orders" do
+        sign_in customer
+        subject
+        expect(assigns(:orders)).to be_a(ActiveRecord::Relation)
+      end
+
+      it "assigns @older_orders_counter" do
+        sign_in customer
+        subject
+        expect(assigns(:older_orders_counter)).to be_a(Fixnum)
+      end
+
+      it "assigns @from_week and @to_week" do
+        sign_in customer
+        subject
+        expect(assigns(:from_week)).to be_a(Fixnum)
+        expect(assigns(:to_week)).to be_a(Fixnum)
+      end
+    end
+    
   
-    it "assigns @orders" do
-      sign_in customer
-      get :analytics
-      expect(assigns(:orders)).to be_a(ActiveRecord::Relation)
-    end
-
-    it "assigns @older_orders_counter" do
-      sign_in customer
-      get :analytics
-      expect(assigns(:older_orders_counter)).to be_a(Fixnum)
-    end
-
-    it "assigns @from_week and @to_week" do
-      sign_in customer
-      get :analytics
-      expect(assigns(:from_week)).to be_a(Fixnum)
-      expect(assigns(:to_week)).to be_a(Fixnum)
-    end
+    
   end
 end
