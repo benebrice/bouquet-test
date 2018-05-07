6f2a749ce69384ec8fb2975ee484d69a0de78172
Brice <brice.bene@gmail.com>
2018-05-07 10:11:27 +0200

    Remove useless files

--- spec/factories/customer_spect.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :customer, class: 'Customer' do |f|
-    password  Faker::Internet.password
-    f.sequence(:email) { |n| "customer_#{n}@theawesomecompany.com" }
-    f.sequence(:first_name) { Faker::Name.first_name }
-  end
-end
